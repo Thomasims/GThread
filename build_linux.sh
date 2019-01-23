561fbcf0127a6ddf0e908d6f2ba101727f5599d1
Thomasims <thomasims3@hotmail.fr>
2019-01-23 11:07:51 +0100

    Dumb workaround to get the module to load on linux

--- build_linux.sh
+++ build_linux.sh
@@ -1,1 +1,1 @@
-gcc -m32 -std=c++14 -shared -o build/gmsv_gthread_linux.so src/*.cpp -Iinclude/ -Llibs/ -llua_shared_32
+gcc -m32 -std=c++14 -shared -o build/gmsv_gthread_linux.so src/*.cpp -Iinclude/ -Llibs/ -l:garrysmod/bin/lua_shared.so
